65add2a22586d2ec552f3922ccf7a8f8114eefba
Andre McMorris <ajmcmorris@gmail.com>
2025-07-17 14:41:52 -0700

    added simple routing to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
 import ExpenseForm from './components/forms/expense-form/expense-form'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-     <ExpenseForm />
-    </>
-  )
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ExpenseForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
