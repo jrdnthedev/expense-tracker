787ff270a588bb3c95d8c8f0e40892562c367e81
Andre McMorris <ajmcmorris@gmail.com>
2025-07-18 11:12:20 -0700

    addded new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import ExpenseForm from './components/forms/expense-form/expense-form'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './components/layout/dashboard/dashboard';
 import BudgetManager from './components/layout/budget-manager/budget-manager';
+import AnalyticsDashboard from './components/layout/analytics-dashboard/analytics-dashboard';
+import Settings from './components/layout/settings/settings';
 
 function App() {
   return (
@@ -11,11 +13,15 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/expenseform">Form</Link>
         <Link to="/budgetmanager">Budget Manager</Link>
+        <Link to="/analytics">Analytics</Link>
+        <Link to="/settings">Settings</Link>
       </nav>
       <Routes>
         <Route path="/expenseform" element={<ExpenseForm />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/budgetmanager" element={<BudgetManager />} />
+        <Route path="/analytics" element={<AnalyticsDashboard />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
     </div>
