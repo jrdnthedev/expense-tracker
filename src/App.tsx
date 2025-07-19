e7ee935fed8d4c8840fb67923446155aff90a4c9
Andre McMorris <ajmcmorris@gmail.com>
2025-07-19 12:01:56 -0700

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import ExpenseForm from './components/forms/expense-form/expense-form'
+import ExpenseForm from './components/forms/expense-form/expense-form';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './components/layout/dashboard/dashboard';
 import BudgetManager from './components/layout/budget-manager/budget-manager';
 import AnalyticsDashboard from './components/layout/analytics-dashboard/analytics-dashboard';
 import Settings from './components/layout/settings/settings';
+import ExpenseList from './components/layout/expense-list/expense-list';
 
 function App() {
   return (
-    <div className='p-4 bg-gray-100 min-h-screen'>
+    <div className="p-4 bg-gray-100 min-h-screen">
       <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/expenseform">Form</Link>
-        <Link to="/budgetmanager">Budget Manager</Link>
-        <Link to="/analytics">Analytics</Link>
-        <Link to="/settings">Settings</Link>
-      </nav>
-      <Routes>
-        <Route path="/expenseform" element={<ExpenseForm />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/budgetmanager" element={<BudgetManager />} />
-        <Route path="/analytics" element={<AnalyticsDashboard />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-    </Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/expenseform">Form</Link>
+          <Link to="/budgetmanager">Budget Manager</Link>
+          <Link to="/analytics">Analytics</Link>
+          <Link to="/settings">Settings</Link>
+          <Link to="/expenselist">Expense List</Link>
+        </nav>
+        <Routes>
+          <Route path="/expenseform" element={<ExpenseForm />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/budgetmanager" element={<BudgetManager />} />
+          <Route path="/analytics" element={<AnalyticsDashboard />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+          <Route path="/expenselist" element={<ExpenseList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
