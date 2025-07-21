c4a075fee39a99423ef7463a2f17e7423a6a0e6f
Andre McMorris <ajmcmorris@gmail.com>
2025-07-20 22:27:00 -0700

    added form

---
+++ src/components/forms/edit-category-form/edit-category-form.tsx
@@ -0,0 +1,10 @@
+export default function EditCategoryForm() {
+  // This component will handle the form for editing categories
+  return (
+    <div className="edit-category-form">
+      {/* Form implementation goes here */}
+      <h2>Edit Category</h2>
+      {/* Add form fields and submit button */}
+    </div>
+  );
+}
