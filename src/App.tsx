ccdb07b2723aafdbac108eb6c469439a90dc9dc1
Andre McMorris <ajmcmorris@gmail.com>
2025-07-23 12:14:06 -0700

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,38 @@ import AnalyticsDashboard from './components/layout/analytics-dashboard/analytic
 import Settings from './components/layout/settings/settings';
 import ExpenseList from './components/layout/expense-list/expense-list';
 import CategoryManagement from './components/layout/category-management/category-management';
+import { AppProvider } from './context/app-state-context';
 
 function App() {
   return (
-    <div className="p-4 bg-gray-100 min-h-screen">
-      <Router>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/expenseform">Form</Link>
-          <Link to="/budgetmanager">Budget Manager</Link>
-          <Link to="/analytics">Analytics</Link>
-          <Link to="/settings">Settings</Link>
-          <Link to="/expenselist">Expense List</Link>
-          <Link to="/categorymanagement">Category Management</Link>
-        </nav>
-        <Routes>
-          <Route path="/expenseform" element={<ExpenseForm />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/budgetmanager" element={<BudgetManager />} />
-          <Route path="/analytics" element={<AnalyticsDashboard />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/expenselist" element={<ExpenseList />} />
-          <Route path="/categorymanagement" element={<CategoryManagement />} />
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-      </Router>
-    </div>
+    <AppProvider>
+      <div className="p-4 bg-gray-100 min-h-screen">
+        <Router>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/expenseform">Form</Link>
+            <Link to="/budgetmanager">Budget Manager</Link>
+            <Link to="/analytics">Analytics</Link>
+            <Link to="/settings">Settings</Link>
+            <Link to="/expenselist">Expense List</Link>
+            <Link to="/categorymanagement">Category Management</Link>
+          </nav>
+          <Routes>
+            <Route path="/expenseform" element={<ExpenseForm />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/budgetmanager" element={<BudgetManager />} />
+            <Route path="/analytics" element={<AnalyticsDashboard />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/expenselist" element={<ExpenseList />} />
+            <Route
+              path="/categorymanagement"
+              element={<CategoryManagement />}
+            />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </Router>
+      </div>
+    </AppProvider>
   );
 }
 
