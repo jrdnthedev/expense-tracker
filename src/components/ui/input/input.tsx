dae7c059b46bb58a0182dd3a6f888f52c2ec8879
Andre McMorris <ajmcmorris@gmail.com>
2025-07-25 21:45:58 -0700

    created new input component

---
+++ src/components/ui/input/input.tsx
@@ -0,0 +1,20 @@
+export default function Input({ value, onChange, placeholder, type, id }: InputProps) {
+    return (
+       <input
+              type={type}
+              className="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
+              id={id}
+              onChange={onChange}
+              value={value}
+              placeholder={placeholder}
+            />
+    );
+}
+
+interface InputProps {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    placeholder?: string;
+    type?: string;
+    id?: string;
+}
