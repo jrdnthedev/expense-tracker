2789e9dbb7055da9198808581c3b8e30cb0b80bd
Andre McMorris <ajmcmorris@gmail.com>
2025-07-25 22:35:18 -0700

    updated component props

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 export default function Button({ children, onClick, type = 'button', primary }: ButtonProps) {
     return (
-        <button onClick={onClick} className={`flex-1 font-semibold py-2 px-4 rounded-md shadow transition-colors cursor-pointer${primary? ' bg-blue-600 hover:bg-blue-700 text-white':' bg-gray-200 hover:bg-gray-300 text-gray-800'}`} type={type}>
+        <button onClick={onClick} className={`flex-1 font-semibold py-2 px-4 rounded-md shadow transition-colors cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 ${primary? ' bg-blue-600 hover:bg-blue-700 text-white':' bg-gray-200 hover:bg-gray-300 text-gray-800'}`} type={type}>
             {children}
         </button>
     );
--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,20 +1,29 @@
-export default function Input({ value, onChange, placeholder, type, id }: InputProps) {
-    return (
-       <input
-              type={type}
-              className="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
-              id={id}
-              onChange={onChange}
-              value={value}
-              placeholder={placeholder}
-            />
-    );
+export default function Input({
+  value,
+  onChange,
+  placeholder,
+  type,
+  id,
+  required
+}: InputProps) {
+  return (
+    <input
+      type={type}
+      className="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
+      id={id}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+      required={required}
+    />
+  );
 }
 
 interface InputProps {
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    placeholder?: string;
-    type?: string;
-    id?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  type?: string;
+  id?: string;
+  required?: boolean;
 }
