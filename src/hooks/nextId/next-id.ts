302cfc77de05a15a6ea81dc5ec7a9ac15cf95fb1
Andre McMorris <ajmcmorris@gmail.com>
2025-07-28 16:19:31 -0700

    created hook to generate next id

---
+++ src/hooks/nextId/next-id.ts
@@ -0,0 +1,8 @@
+import { useMemo } from "react";
+
+export function useNextId<T extends { id: number }>(items: T[]) {
+  return useMemo(() => {
+    if (!items || items.length === 0) return 1;
+    return Math.max(...items.map(item => item.id)) + 1;
+  }, [items]);
+}
