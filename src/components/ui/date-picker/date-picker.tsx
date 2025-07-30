468371fcc296f42d047c44c5826de554f2def92d
Andre McMorris <ajmcmorris@gmail.com>
2025-07-30 14:53:04 -0700

    added date-picker tests

--- src/components/ui/date-picker/date-picker.tsx
+++ src/components/ui/date-picker/date-picker.tsx
@@ -7,6 +7,7 @@ export default function DatePicker({
   return (
     <input
       type="date"
+      role="textbox"
       className="w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer"
       id={id}
       defaultValue={defaultValue}
