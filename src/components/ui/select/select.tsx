991d81b5b615997bb9221e2d12dd6b2844167447
Andre McMorris <ajmcmorris@gmail.com>
2025-08-03 20:05:07 -0700

    added style update for lingering focus ring issue

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,20 +1,39 @@
-export default function Select<T>({ name, id, options, value, onChange,getOptionValue, getOptionLabel,getOptionId }: SelectProps<T>) {
-
+export default function Select<T>({
+  name,
+  id,
+  options,
+  value,
+  onChange,
+  getOptionValue,
+  getOptionLabel,
+  getOptionId,
+}: SelectProps<T>) {
   return (
     <select
       name={name}
       id={id}
       value={value}
-      onChange={e => {
+      onChange={(e) => {
         const selectedIndex = e.target.selectedIndex;
         const selectedOption = e.target.options[selectedIndex];
         const dataId = selectedOption.getAttribute('data-id');
         onChange(e.target.value, Number(dataId));
       }}
-      className="w-full border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="w-full border border-gray-300 rounded-md px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
+        active:outline-none active:border-transparent
+        cursor-pointer
+        [-webkit-appearance:none]
+        [&:-webkit-autofill]:bg-white
+        [&:-webkit-autofill:focus]:bg-white
+        [&:-webkit-autofill:active]:bg-white"
+      autoComplete="off"
     >
       {options.map((option: T) => (
-        <option key={getOptionValue(option)} value={getOptionValue(option)} data-id={getOptionId(option)}>
+        <option
+          key={getOptionValue(option)}
+          value={getOptionValue(option)}
+          data-id={getOptionId(option)}
+        >
           {getOptionLabel(option)}
         </option>
       ))}
