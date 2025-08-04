55613d468038e3147902c092ed9d66d30e9ba075
Andre McMorris <ajmcmorris@gmail.com>
2025-08-03 22:28:02 -0700

    added currency formatting to charts

--- src/components/charts/bar-chart/bar-chart.tsx
+++ src/components/charts/bar-chart/bar-chart.tsx
@@ -7,23 +7,25 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from 'recharts';
+import { useAppState } from '../../../context/app-state-hooks';
+import { formatAmount } from '../../../utils/currency';
 
 export default function CustomBarChart<T>({ data }: ChartProps<T>) {
+  const { currency } = useAppState();
   const colors = ['#6366F1', '#10B981', '#EF4444', '#F59E0B', '#8B5CF6'];
-  const dataKeys = Object.keys(data[0] || {}).filter(key => key !== 'name');
+  const dataKeys = Object.keys(data[0] || {}).filter((key) => key !== 'name');
+  const formatTooltipValue = (value: number) => {
+    return formatAmount(value, currency);
+  };
   return (
     <ResponsiveContainer width="100%" height={300}>
       <BarChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
         <YAxis />
-        <Tooltip />
+        <Tooltip formatter={formatTooltipValue} />
         {dataKeys.map((key, index) => (
-          <Bar
-            key={key}
-            dataKey={key}
-            fill={colors[index % colors.length]}
-          />
+          <Bar key={key} dataKey={key} fill={colors[index % colors.length]} />
         ))}
       </BarChart>
     </ResponsiveContainer>
