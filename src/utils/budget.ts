83077b108aa016c8cb7d7f2a8ef6c6550c50f7a6
Andre McMorris <ajmcmorris@gmail.com>
2025-08-04 10:57:31 -0700

    updated calculate function

--- src/utils/budget.ts
+++ src/utils/budget.ts
@@ -9,12 +9,7 @@ export function getBudgetStartDate(categoryId: number, budgets: Budget[]): strin
   };
 
 export function calculateAllRemainingBudgets(budgets: Budget[], expenses: Expense[]): number {
-  // Calculate total budget limit
   const totalBudget = budgets.reduce((total, budget) => total + budget.limit, 0);
-  
-  // Calculate total expenses
   const totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
-  
-  // Return difference
   return totalBudget - totalExpenses;
 }
